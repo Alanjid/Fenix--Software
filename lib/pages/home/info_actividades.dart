3d6eb748dca55d9bd3b59e78d09ddfad3c4b7e2f
SpiiderJoss <montalvanzavala@gmail.com>
2023-10-16 12:33:03 -0600

    Ventana de info de actividades

--- lib/pages/home/info_actividades.dart
+++ lib/pages/home/info_actividades.dart
@@ -15,40 +15,139 @@ class info_pictogramas extends StatelessWidget{
                  return AlertDialog(
                       title: Text("Pictogramas"),
                    content: Column(
-                     mainAxisSize: MainAxisSize.min,
+                     mainAxisSize: MainAxisSize.max,
                      children: [
                           Container(
-                            width: 500,
-                            height: 200,
+                            width: 600,
+                            height: 300,
                             color: Colors.white,
                             child: Row(
                               children: [
-                                Column(
+                                Container(
+                                  width: 300,
+                                  height: 300,
+                                  child: Column(
                                     children: [
-                                      Container(
-                                        width: 70,
-                                        height: 70,
-                                        decoration: BoxDecoration(
-                                          border: Border.all(color: Colors.black, width: 2.0), // Configura el borde negro
-                                        ),
-                                        child: Image(
-                                          image: AssetImage("assets/img/alimento.png"),
-                                        ),
-                                      ),
-                                      Container(
-                                        width: 70,
-                                        height: 70,
-                                        decoration: BoxDecoration(
-                                          border: Border.all(color: Colors.black, width: 2.0), // Configura el borde negro
-                                        ),
-                                        child: Image(
-                                          image: AssetImage("assets/img/bebidas.png"),
-                                        ),
-                                      )
-                                    ],
-                                ),
-                                Column(
+                                      Column(
+                                        children:
+                                        [
+                                          SizedBox(height: 20,),
+                                          Row(
+                                            children: [
+                                              Container(
+                                                width: 50,
+                                                height: 50,
+                                                decoration: BoxDecoration(
+                                                  border: Border.all(color: Colors.black, width: 2.0), // Configura el borde negro
+                                                ),
+                                                child: Image(
+                                                  image: AssetImage("assets/img/alimento.png"),
+                                                ),
+                                              ),
+                                              SizedBox(width: 20,),
+                                              Text("ALIMENTOS"),
 
+                                            ],
+                                          ),
+                                          SizedBox(height: 20,),
+                                          Row(
+                                            children: [
+                                              Container(
+                                                width: 50,
+                                                height: 50,
+                                                decoration: BoxDecoration(
+                                                  border: Border.all(color: Colors.black, width: 2.0), // Configura el borde negro
+                                                ),
+                                                child: Image(
+                                                  image: AssetImage("assets/img/bebidas.png"),
+                                                ),
+                                              ),
+                                              SizedBox(width: 20,),
+                                              Text("BEBIDAS"),
+
+                                            ],
+                                          ),
+                                          SizedBox(height: 20),
+                                          Row(
+                                            children: [
+                                              Container(
+                                                width: 50,
+                                                height: 50,
+                                                decoration: BoxDecoration(
+                                                  border: Border.all(color: Colors.black, width: 2.0), // Configura el borde negro
+                                                ),
+                                                child: Image(
+                                                  image: AssetImage("assets/img/acciones.png"),
+                                                ),
+                                              ),
+                                              SizedBox(width: 20,),
+                                              Text("ACCIONES"),
+
+                                            ],
+                                          ),
+                                          SizedBox(height: 20,),
+                                          Row(
+                                            children: [
+                                              Container(
+                                                width: 50,
+                                                height: 50,
+                                                decoration: BoxDecoration(
+                                                  border: Border.all(color: Colors.black, width: 2.0), // Configura el borde negro
+                                                ),
+                                                child: Image(
+                                                  image: AssetImage("assets/img/partes del cuerpo.png"),
+                                                ),
+                                              ),
+                                              SizedBox(width: 20,),
+                                              Text("PARTES DEL CUERPO"),
+
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  width: 300,
+                                  height: 300,
+                                  child: Column(
+                                    children: [
+                                      Row(
+                                        children: [
+                                          SizedBox(height: 90,),
+                                          Container(
+                                            width: 50,
+                                            height: 50,
+                                            decoration: BoxDecoration(
+                                              border: Border.all(color: Colors.black, width: 2.0), // Configura el borde negro
+                                            ),
+                                            child: Image(
+                                              image: AssetImage("assets/img/prendas.png"),
+                                            ),
+                                          ),
+                                          SizedBox(width: 20,),
+                                          Text("PRENDAS DE VESTIR"),
+
+                                        ],
+                                      ),
+                                      SizedBox(height: 170,),
+                                      Row(
+                                        children: [
+                                          SizedBox(
+                                            width: 200,
+                                          ),
+                                          ElevatedButton(
+                                            child: const Text("CERRAR"),
+                                            onPressed: (){
+                                              Navigator.pop(context);
+                                            },
+
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ],
                             ),
